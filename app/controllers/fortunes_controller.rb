65580f696d47c3f39eff7a6d2571678b1325dddf
JackRB52 <jbouchard@chicagobooth.edu>
2022-04-27 21:55:19 +0000

    dynamic refactoring

---
+++ app/controllers/fortunes_controller.rb
@@ -0,0 +1,18 @@
+ class Fortunes < ApplicationController
+  def horoscopes
+    @sign = params.fetch(:the_sign).to_sym
+    @sign_name = params.fetch(:the_sign).capitalize
+
+    all_zodiacs = Zodiac.list
+    this_zodiac = all_zodiacs.fetch(@sign)
+    @horoscope = this_zodiac.fetch(:horoscope)
+
+    5.times do
+      another_number = rand(1...100)
+      
+      @array_of_numbers.push(another_number)
+    end
+
+    render({ :template => "zodiac_templates/sign.html.erb" })
+  end
+end
